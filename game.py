a2aaef0a1da0654226c7ea3c06907d83a22cd85a
aliciathoe <thoeal112400@cvh.rusd.us>
2017-03-28 11:17:19 -0700

    first change

---
+++ game.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+TTT = Tk()
+
+b1 = Button(TTT, bg = "blue", height = 2, width = 4)
+b1.pack(padx=1, pady = 1)
+b2 = Button(TTT,bg = "red", height = 2, width = 4)
+b2.pack(pady = 0, padx = 1)
+b3 = Button(TTT, bg = "green", height = 2, width = 4)
+b3.pack()
+b4 = Button(TTT, bg = "red", height = 2, width = 4)
+b4.pack()
+b5 = Button(TTT, bg = "green", height = 2, width = 4)
+b5.pack()
+b6 = Button(TTT, bg = "blue", height = 2, width = 4)
+b6.pack()
+b7 = Button(TTT, bg = "green", height = 2, width = 4)
+b7.pack()
+b8 = Button(TTT, bg = "blue", height = 2, width = 4)
+b8.pack()
+b9 = Button(TTT, bg = "red", height = 2, width = 4)
+b9.pack()
+
+TTT.mainloop()
