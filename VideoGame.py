8ad9dfb5dcfde8dc4930b8d034aa2e2de523df77
aliciathoe <thoeal112400@cvh.rusd.us>
2017-04-21 10:45:09 -0700

    update

--- VideoGame.py
+++ VideoGame.py
@@ -17,18 +17,51 @@ pygame.draw.line(gameDisplay, white, (300, 100), (300,400), 2)
 pygame.display.update()
 
 gameExit = False
+keys = pygame.key.get_pressed()
 
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            pygame.draw.line(gameDisplay, white, (100, 200), (200, 300),2)
-            pygame.draw.line(gameDisplay, white, (200, 200), (100, 300), 2)
-            pygame.display.update()
-        elif event.type == pygame.KEYDOWN:
-            pygame.draw.circle(gameDisplay, white, (200,200), 50, 2)
-            pygame.display.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                    pygame.draw.line(gameDisplay, white, (100, 100), (200, 200),2)
+                    pygame.draw.line(gameDisplay, white, (200, 100), (100, 200), 2)
+                    pygame.display.update()
+            if event.key == pygame.K_2:
+                pygame.draw.line(gameDisplay, white, (200, 100), (300, 200),2)
+                pygame.draw.line(gameDisplay, white, (300, 100), (200, 200), 2)
+                pygame.display.update()
+            if event.key == pygame.K_3:
+                pygame.draw.line(gameDisplay, white, (300, 100), (400, 200),2)
+                pygame.draw.line(gameDisplay, white, (400, 100), (300, 200), 2)
+                pygame.display.update()
+            if event.key == pygame.K_4:
+                pygame.draw.line(gameDisplay, white, (100, 200), (200, 300),2)
+                pygame.draw.line(gameDisplay, white, (200, 200), (100, 300), 2)
+                pygame.display.update()
+            if event.key == pygame.K_5:
+                pygame.draw.line(gameDisplay, white, (200, 200), (300, 300),2)
+                pygame.draw.line(gameDisplay, white, (300, 200), (200, 300), 2)
+                pygame.display.update()
+            if event.key == pygame.K_6:
+                pygame.draw.line(gameDisplay, white, (300, 200), (400, 300),2)
+                pygame.draw.line(gameDisplay, white, (400, 200), (300, 300), 2)
+                pygame.display.update()
+            if event.key == pygame.K_7:
+                pygame.draw.line(gameDisplay, white, (100, 300), (200, 400),2)
+                pygame.draw.line(gameDisplay, white, (200, 300), (100, 400), 2)
+                pygame.display.update()
+            if event.key == pygame.K_8:
+                pygame.draw.line(gameDisplay, white, (200, 300), (300, 400),2)
+                pygame.draw.line(gameDisplay, white, (300, 300), (200, 400), 2)
+                pygame.display.update()
+            if event.key == pygame.K_9:
+                pygame.draw.line(gameDisplay, white, (300, 300), (400, 400),2)
+                pygame.draw.line(gameDisplay, white, (400, 300), (300, 400), 2)
+                pygame.display.update()
+
+
 
 pygame.display.update()
 
