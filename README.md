9d0dfd43adb5b1eae15d04620fadd1b827647b68
aliciathoe <thoeal112400@cvh.rusd.us>
2017-04-25 11:16:47 -0700

    Instructions and Citations

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TicTacToe
-This is my GUI Project.
+This is my pygame version of Tic Tac Toe.
+
+Player One(X) uses the numbers at the top of the keyboard and presses the corresponding number on the keyboard in order to make their move.
+
+Player Two(O) will then use the keypad to the side and press the corresponding number on the keyboard to draw their circle.
+
+The game continues until someone gets three in a row, thus becoming the winner.
+
+
+Cites
+http://stackoverflow.com/questions/21707337/pygame-font-font-render-have-typeerror
+  Font type error 
+
+https://www.youtube.com/playlist?list=PL6gx4Cwl9DGAjkwJocj7vlc_mFU-4wXJq
+    This is a Youtube playlist made by TheNewBoston that explains the basic necessities of the pygame module. While the tutorials follow a snake game, these lessons can be applied to any sort of game. The first few are simply for the setup of pygame.
+
+https://www.pygame.org/docs/ref/surface.html
+    I used this to find commands to use for my game that I had not yet known from the tutorial that I watched.
--- VideoGame.py
+++ VideoGame.py
@@ -7,11 +7,21 @@ white = (255, 255, 255)
 gameDisplay = pygame.display.set_mode((500,500))
 pygame.display.set_caption('Tic Tac Toe')
 
+#Text
+font = pygame.font.SysFont("Arial", 50, True, False)
+text = font.render("1", True, white, None)
+
 #Game Board
 pygame.draw.line(gameDisplay, white, (100, 200), (400,200), 2)
 pygame.draw.line(gameDisplay, white, (100, 300), (400,300), 2)
 pygame.draw.line(gameDisplay, white, (200, 100), (200,400), 2)
 pygame.draw.line(gameDisplay, white, (300, 100), (300,400), 2)
+
+pygame.font.init()
+
+myfont = pygame.font.SysFont('Arial',30)
+textsurface = myfont.render('1', False, (0, 0 , 0))
+gameDisplay.blit(textsurface, (0, 0))
 
 
 pygame.display.update()
@@ -19,20 +29,21 @@ pygame.display.update()
 gameExit = False
 keys = pygame.key.get_pressed()
 
+#Game
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
+            if event.key == pygame.K_7:
                     pygame.draw.line(gameDisplay, white, (100, 100), (200, 200),2)
                     pygame.draw.line(gameDisplay, white, (200, 100), (100, 200), 2)
                     pygame.display.update()
-            if event.key == pygame.K_2:
+            if event.key == pygame.K_8:
                 pygame.draw.line(gameDisplay, white, (200, 100), (300, 200),2)
                 pygame.draw.line(gameDisplay, white, (300, 100), (200, 200), 2)
                 pygame.display.update()
-            if event.key == pygame.K_3:
+            if event.key == pygame.K_9:
                 pygame.draw.line(gameDisplay, white, (300, 100), (400, 200),2)
                 pygame.draw.line(gameDisplay, white, (400, 100), (300, 200), 2)
                 pygame.display.update()
@@ -48,28 +59,48 @@ while not gameExit:
                 pygame.draw.line(gameDisplay, white, (300, 200), (400, 300),2)
                 pygame.draw.line(gameDisplay, white, (400, 200), (300, 300), 2)
                 pygame.display.update()
-            if event.key == pygame.K_7:
+            if event.key == pygame.K_1:
                 pygame.draw.line(gameDisplay, white, (100, 300), (200, 400),2)
                 pygame.draw.line(gameDisplay, white, (200, 300), (100, 400), 2)
                 pygame.display.update()
-            if event.key == pygame.K_8:
+            if event.key == pygame.K_2:
                 pygame.draw.line(gameDisplay, white, (200, 300), (300, 400),2)
                 pygame.draw.line(gameDisplay, white, (300, 300), (200, 400), 2)
                 pygame.display.update()
-            if event.key == pygame.K_9:
+            if event.key == pygame.K_3:
                 pygame.draw.line(gameDisplay, white, (300, 300), (400, 400),2)
                 pygame.draw.line(gameDisplay, white, (400, 300), (300, 400), 2)
                 pygame.display.update()
-
+            if event.key == pygame.K_KP7:
+                pygame.draw.circle(gameDisplay, white, (150,150), 50, 2)
+                pygame.display.update()
+            if event.key == pygame.K_KP8:
+                pygame.draw.circle(gameDisplay, white, (250,150), 50, 2)
+                pygame.display.update()
+            if event.key == pygame.K_KP9:
+                pygame.draw.circle(gameDisplay, white, (350,150), 50, 2)
+                pygame.display.update()
+            if event.key == pygame.K_KP4:
+                pygame.draw.circle(gameDisplay, white, (150,250), 50, 2)
+                pygame.display.update()
+            if event.key == pygame.K_KP5:
+                pygame.draw.circle(gameDisplay, white, (250,250), 50, 2)
+                pygame.display.update()
+            if event.key == pygame.K_KP6:
+                pygame.draw.circle(gameDisplay, white, (350,250), 50, 2)
+                pygame.display.update()
+            if event.key == pygame.K_KP1:
+                pygame.draw.circle(gameDisplay, white, (150,350), 50, 2)
+                pygame.display.update()
+            if event.key == pygame.K_KP2:
+                pygame.draw.circle(gameDisplay, white, (250,350), 50, 2)
+                pygame.display.update()
+            if event.key == pygame.K_KP3:
+                pygame.draw.circle(gameDisplay, white, (350,350), 50, 2)
+                pygame.display.update()
 
 
 pygame.display.update()
 
 pygame.quit()
 quit()
-
-
-#try to find a way to run a function with only specific codes so yu can
-#assign each number with its own square
-
-#https://www.pygame.org/docs/ref/event.html
